fb6742c65ccffc2bffc88d86fe2e5626b2e5d1a3
KZeillmann <kzeillmann@gmail.com>
2016-07-26 23:45:41 -0500

    Refactor bootstrap so that it's installed via NPM and not just plain text in the app.css file

--- brunch-config.js
+++ brunch-config.js
@@ -65,6 +65,7 @@ exports.config = {
     enabled: true,
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html"]
+    whitelist: ["phoenix", "phoenix_html"],
+    styles: {bootstrap: ["dist/css/bootstrap.css"]}
   }
 };
