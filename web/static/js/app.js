8011517c40f7307ccd9988e00d85758e4d09e11c
KZeillmann <kzeillmann@gmail.com>
2016-08-06 10:54:39 -0500

    Downgrading bootstrap so that JS works

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,9 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+global.jQuery = require("jquery")
+global.bootstrap = require("bootstrap")
+
 // Import local files
 //
 // Local files can be imported directly using relative
