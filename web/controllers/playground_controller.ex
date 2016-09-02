07d16a9fed01c429ba6d2305d4fd5625a27688ed
KZeillmann <kzeillmann@gmail.com>
2016-09-01 23:50:10 -0500

    Add dropdown for playground so we can choose datasource from there

--- web/controllers/playground_controller.ex
+++ web/controllers/playground_controller.ex
@@ -1,14 +1,20 @@
 defmodule Ironclad.PlaygroundController do
   use Ironclad.Web, :controller
+  alias Ironclad.Database
 
   def index(conn, _params) do
-    render conn, "index.html"
+    query = from d in "databases",
+            select: d.name
+    databases = Repo.all(query)
+    IO.inspect databases
+    render conn, "index.html", databases: databases
   end
 
   def query(conn, %{"query" => query}) do
-    {:ok, pid} = Postgrex.start_link(hostname: query["connection_string"], database: query["database"], username: query["username"], password: query["password"])
+    datasource = Repo.get_by(Database, name: query["datasource"])
+
+    {:ok, pid} = Postgrex.start_link(hostname: datasource.location, database: datasource.database, username: datasource.username, password: datasource.password)
     query_result = Postgrex.query!(pid, query["query_text"], [])
-    # IO.inspect query_result
     render conn, "result.html", query_result: query_result
   end
 end
